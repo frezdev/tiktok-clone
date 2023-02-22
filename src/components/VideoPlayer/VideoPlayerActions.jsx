f7646e6365979ae8c0e04c3821bb840a7b4c8f2e
carlosandf <carlos.andres.ff2912@gmail.com>
2023-02-22 18:08:55 -0500

    feat(Root): Start working on Videos Actions Sidebar

---
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -0,0 +1,20 @@
+import { Heart } from '../icons/Heart'
+import styles from './styles.module.css'
+
+export default function VideoPlayerActions () {
+  return (
+    <aside className={styles.actions}>
+      <div className={styles.action}>
+        <Heart></Heart>
+      </div>
+
+      <div className={styles.action}>
+        <Heart></Heart>
+      </div>
+
+      <div className={styles.action}>
+        <Heart></Heart>
+      </div>
+    </aside>
+  )
+}
--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,31 +1,40 @@
 import { useRef, useState } from 'react'
-import videoSrc from '../../../assets/video.mp4'
+import clsx from 'clsx'
 import styles from './styles.module.css'
+import VideoPlayerActions from './VideoPlayerActions'
 
-export default function VideoPlayer () {
+export default function VideoPlayer ({ src }) {
   const [playing, setPlaying] = useState(false)
-  const video = useRef()
+  const video = useRef(null)
 
   const handlePlay = () => {
+    const { current: videoEl } = video
     playing
-      ? video.current.pause()
-      : video.current.play()
+      ? videoEl.pause()
+      : videoEl.play()
 
     setPlaying(!playing)
   }
 
+  const playerClassName = clsx(styles.player, {
+    [styles.hidden]: playing
+  })
+
   return (
-    <>
+    <div className={styles.wrapper}>
       <video
+        onClick={handlePlay}
         className={styles.video}
-        src={videoSrc}
+        src={src}
+        loop
         controls={false}
         ref={video}
       />
-      <button
-        className={styles.player}
+      <i
+        className={playerClassName}
         onClick={handlePlay}
       />
-    </>
+      <VideoPlayerActions />
+    </div>
   )
 }
