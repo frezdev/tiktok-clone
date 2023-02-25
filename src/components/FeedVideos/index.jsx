b8159969de007533e340856faa699143cb7fac91
carlosandf <carlos.andres.ff2912@gmail.com>
2023-02-24 19:59:33 -0500

    feat(Root): Add another data

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import videoSrc from '../../../assets/video.mp4'
+import videoSrc2 from '../../../assets/video2.mp4'
+import albumCoverImg from '../../../assets/albumCoverImg.jpg'
 import VideoPlayer from '../VideoPlayer'
 import styles from './styles.module.css'
 
@@ -11,42 +13,20 @@ const VIDEOS = [
     likes: 123,
     shares: 234,
     songTitle: 'sonido original - leidyceledon',
-    albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/9e142ff7c726a4e58180028cbd334166~c5_100x100.jpeg?x-expires=1677261600&x-signature=SUNbJqLGzn5TQ2lCDmTZcblZ02E%3D',
+    albumCover: albumCoverImg,
     comments: 357,
     src: videoSrc
   },
   {
     id: 2,
-    author: 'leidyceledon',
+    author: 'yenrva',
     description: '2 meses de Argentina vs Croacia | no copyright infringement #messi #argentina #fypシ #parati #julianalvarez #messifans #foryou #scaloneta #fyp #modric',
     likes: 123,
     shares: 234,
     songTitle: 'sonido original - leidyceledon',
-    albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/9e142ff7c726a4e58180028cbd334166~c5_100x100.jpeg?x-expires=1677261600&x-signature=SUNbJqLGzn5TQ2lCDmTZcblZ02E%3D',
+    albumCover: albumCoverImg,
     comments: 357,
-    src: videoSrc
-  },
-  {
-    id: 3,
-    author: 'leidyceledon',
-    description: '2 meses de Argentina vs Croacia | no copyright infringement #messi #argentina #fypシ #parati #julianalvarez #messifans #foryou #scaloneta #fyp #modric',
-    likes: 123,
-    shares: 234,
-    songTitle: 'sonido original - leidyceledon',
-    albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/9e142ff7c726a4e58180028cbd334166~c5_100x100.jpeg?x-expires=1677261600&x-signature=SUNbJqLGzn5TQ2lCDmTZcblZ02E%3D',
-    comments: 357,
-    src: videoSrc
-  },
-  {
-    id: 4,
-    author: 'leidyceledon',
-    description: '2 meses de Argentina vs Croacia | no copyright infringement #messi #argentina #fypシ #parati #julianalvarez #messifans #foryou #scaloneta #fyp #modric',
-    likes: 123,
-    shares: 234,
-    songTitle: 'sonido original - leidyceledon',
-    albumCover: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/9e142ff7c726a4e58180028cbd334166~c5_100x100.jpeg?x-expires=1677261600&x-signature=SUNbJqLGzn5TQ2lCDmTZcblZ02E%3D',
-    comments: 357,
-    src: videoSrc
+    src: videoSrc2
   }
 ]
 
