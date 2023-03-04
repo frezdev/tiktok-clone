30422a8ad984be681ee59589dfe1aa5274df972a
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-04 17:29:45 -0500

    feat(components): add avatar icon and follow icon

--- src/components/VideoPlayerActions/index.jsx
+++ src/components/VideoPlayerActions/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Heart } from '../icons/Heart'
 import { Comment } from '../icons/Comment'
 import { Share } from '../icons/Share'
+import { FollowIcon } from '../icons/FollowIcon'
+import notProfilePhoto from '../../../assets/not-profile-photo.png'
 import styles from './styles.module.css'
 import AlbumDisk from '../icons/AlbumDisk'
 
@@ -11,7 +13,8 @@ export default function VideoPlayerActions (props) {
     comments = 550,
     shares = 500,
     hearted = false,
-    albumCover
+    albumCover,
+    user
   } = props
   const [liked, setLiked] = useState(false)
 
@@ -30,6 +33,23 @@ export default function VideoPlayerActions (props) {
 
   return (
     <aside className={styles.actions}>
+      <div className={styles.avatarContainer}>
+        <a
+          href={`/user/${user.username}`}
+          className={styles.avatar}
+          style={{ backgroundImage: `url(${user.avatar ? user.avatar : notProfilePhoto})` }}
+        />
+        <FollowIcon
+          handleClick={() => window.alert('Following')}
+          styles={{
+            position: 'absolute',
+            bottom: '-20%',
+            width: '28',
+            height: '28',
+            zIndex: 50
+          }}
+        />
+      </div>
       <div style={{ marginBottom: '49px' }}>
         <button onClick={handleLike} className={styles.action}>
           <Heart fill={(hearted || liked) ? '#fe2c55' : 'currentColor'} width='40' />
