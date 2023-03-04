ebaf27a12999b0536dd83f083cc5a4f7fe5de222
carlosandf <carlos.andres.ff2912@gmail.com>
2023-03-04 17:31:42 -0500

    fix(component): Change class name add prop name

--- src/components/VideoDescription/index.jsx
+++ src/components/VideoDescription/index.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import SongTicker from '../SongTicker'
 import styles from './styles.module.css'
 
-export default function VideoDescription ({ author, description, songTitle }) {
+export default function VideoDescription ({ user, description, songTitle }) {
   return (
     <div className={styles.description}>
       <div className={styles.textWrapper}>
         <strong>
-          <a className={styles.author} href={`/user/${author}`}>
-            @{author}
+          <a className={styles.username} href={`/user/${user.username}`}>
+            @{user.username}
           </a>
         </strong>
         <p className={styles.text}>
